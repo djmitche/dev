ab2641bcc08b6b5e72e68ae3759fb5ae767cf73c
Dustin J. Mitchell <dustin@v.igoro.us>
2007-06-28 04:39:04 +0000

    misc bugfixes

--- extensions/cvs/require.sh
+++ extensions/cvs/require.sh
@@ -1,7 +1,22 @@
 # do a CVS checkout 
 # cvs_checkout REPOSITORY MODULE DIRNAME
 cvs_checkout() {
-  (
-    $CVS -d "$1" checkout -d "$DEV_TASK_DIR/$3" "$2"
-  )
+    local repo module dirname parentdir
+    parentdir="$DEV_TASK_DIR"
+    repo="$1"
+    module="$2"
+    codir="$3"
+
+    test $# -eq 3 || die "cvs_checkout requires three arguments"
+
+    # cvs doesn't handle checking out to '.' properly, so fake it.
+    if test "$codir" = "."; then
+        codir=`$BASENAME $parentdir`
+        parentdir=`$DIRNAME $parentdir`
+    fi
+
+    (
+        cd "$parentdir"
+        $CVS -d "$repo" checkout -d "$codir" "$module"
+    )
 }
