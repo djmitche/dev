0497204e157a60974f3f23bae87a5b826acff021
Dustin J. Mitchell <dustin@v.igoro.us>
2007-07-28 18:46:37 +0000

    (closes #14) make 'quilt' task functions take a target directory

--- extensions/quilt/require.sh
+++ extensions/quilt/require.sh
@@ -3,9 +3,14 @@
 export QUILT_PATCHES
 
 # set up quilt, using the 'series' file in the task directory
-# use_quilt 
+# use_quilt [DIR]
 use_quilt() {
-  ( cd $DEV_TASK_DIR
+  local destdir="$1"
+
+  ( cd "$DEV_TASK_DIR"
+    if test -n "$destdir"; then
+      cd "$destdir"
+    fi
     # quilt looks for 'patches' directory and for '.pc/series'
 
     test -e "${QUILT_PATCHES-patches}" && die "Task already has a patches/ directory"
@@ -25,9 +30,15 @@ use_quilt() {
 
 # unload quilt by popping all of the patches; this will abort an unload
 # if a patch does not unapply
-# unload_quilt
+# unload_quilt [DIR]
 unload_quilt() {
-  ( cd $DEV_TASK_DIR
+  local destdir="$1"
+
+  ( cd "$DEV_TASK_DIR"
+    if test -n "$destdir"; then
+      cd "$destdir"
+    fi
+
     $QUILT pop -a
   )
   # quilt pop returns '2' if there are no patches to pop
@@ -37,7 +48,13 @@ unload_quilt() {
 }
 
 quilt_status() {
-  ( cd $DEV_TASK_DIR
+  local destdir="$1"
+
+  ( cd "$DEV_TASK_DIR"
+    if test -n "$destdir"; then
+      cd "$destdir"
+    fi
+
     echo "Applied patches:"
     $QUILT applied | $SED -e "s/^/  /"
     echo "Unapplied patches:"
