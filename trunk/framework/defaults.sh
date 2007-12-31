d0d83fd27063a36683719b19c177d587701a8f1a
djmitche <djmitche@c63a82ae-f72f-0410-998e-01e90ad1ec64>
2007-12-31 15:58:35 +0000

    Significant rewrite to use [TaskType]s, and to collect base subcommands into subsubcommands

--- trunk/framework/defaults.sh
+++ trunk/framework/defaults.sh
@@ -1,7 +1,7 @@
 # Default values for configuration variables
 
 export DEV_SUBCOMMANDS_PATH="${DEV_PROJECT_DIR}/dev/subcommands"
-export DEV_TASKS_DIR="${DEV_PROJECT_DIR}/dev/tasks"
+export DEV_TASKTYPES_DIR="${DEV_PROJECT_DIR}/dev/tasks"
 
 # Load all extensions' default values
 for ext in $EXTENSIONS; do
