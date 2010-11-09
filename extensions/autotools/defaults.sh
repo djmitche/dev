30db61ab5c0a03cf19974533bfc1a28c665db526
Dustin J. Mitchell <dustin@v.igoro.us>
2010-11-09 22:11:11 +0000

    (Issue #34) make dev var show *all* variables from all enabled extns

--- extensions/autotools/defaults.sh
+++ extensions/autotools/defaults.sh
@@ -1,8 +1,10 @@
 # The build directory, where compilation will take place
 export AUTOTOOLS_BUILD_DIR=${DEV_PROJECT_DIR}/tmp/build
+add_interesting_variable AUTOTOOLS_BUILD_DIR
 
 # The --prefix passed to ./configure
 export AUTOTOOLS_PREFIX_DIR=${DEV_PROJECT_DIR}/tmp/prefix
+add_interesting_variable AUTOTOOLS_PREFIX_DIR
 
 # command to run to set up a directory for autotools (e.g., 'autogen')
 # if you need more than one command here, define a shell function and
@@ -17,9 +19,12 @@ autotools_setup_default() {
     done
 }
 export AUTOTOOLS_SETUP=autotools_setup_default
+add_interesting_variable AUTOTOOLS_SETUP
 
 # ./configure flags
 export AUTOTOOLS_CONFIGURE_FLAGS=
+add_interesting_variable AUTOTOOLS_CONFIGURE_FLAGS
 
-# ./configure script (useful if the script is in a subdirectory
+# ./configure script (useful if the script is in a subdirectory)
 export AUTOTOOLS_CONFIGURE_COMMAND=configure
+add_interesting_variable AUTOTOOLS_CONFIGURE_COMMAND
