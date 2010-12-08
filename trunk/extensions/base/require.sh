d2e0b67d1eaa764ff4c4aa171584882870c6286b
djmitche <djmitche@c63a82ae-f72f-0410-998e-01e90ad1ec64>
2010-12-08 02:56:02 +0000

    Switch to using a function to define tasktypes
    Fixes issue #43

--- trunk/extensions/base/require.sh
+++ trunk/extensions/base/require.sh
@@ -40,3 +40,19 @@ edit_file() {
     test "$EDITOR" != "" || die "\$EDITOR is not defined"
     $EDITOR "$1"
 }
+
+# find a loader script, given its name
+#
+# Input: loader name
+# Output:
+#  - path to loader if found, or empty string
+# Side effects:
+#  - error messages to stderr
+base_find_loader() {
+    local loader_name="$1"
+    if ! test -f "$loaderdir/$loader_name"; then
+        echo "No such loader $loader_name" >&2
+        return 1
+    fi
+    echo "$loaderdir/$loader_name"
+}
