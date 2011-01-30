a2e644677d9d9a92553b5e307dbfdd4d9220f4ba
Dustin J. Mitchell <dustin@v.igoro.us>
2011-01-30 01:52:46 +0000

    add 'color' extension

---
+++ extensions/color/require.sh
@@ -0,0 +1,36 @@
+# define a few colors for use in echo commands
+_ESC=$'\033'
+
+# revert to normal colors
+NORM="$_ESC[0m"
+
+# plain
+BLACK="$_ESC[0;30m"
+BLUE="$_ESC[0;34m"
+GREEN="$_ESC[0;32m"
+CYAN="$_ESC[0;36m"
+RED="$_ESC[0;31m"
+PURPLE="$_ESC[0;35m"
+BROWN="$_ESC[0;33m"
+LTGRAY="$_ESC[0;37m"
+BLACK="$_ESC[0;30m"
+
+# bold
+DKGRAY="$_ESC[1;34m"
+LTBLUE="$_ESC[1;32m"
+LTCYAN="$_ESC[1;36m"
+LTRED="$_ESC[1;31m"
+LTPRUPLE="$_ESC[1;35m"
+YELLOW="$_ESC[1;33m"
+WHITE="$_ESC[1;37m"
+
+# background
+BG_BLACK="$_ESC[40m"
+BG_BLUE="$_ESC[44m"
+BG_GREEN="$_ESC[42m"
+BG_CYAN="$_ESC[46m"
+BG_RED="$_ESC[41m"
+BG_PURPLE="$_ESC[45m"
+BG_BROWN="$_ESC[43m"
+BG_LTGRAY="$_ESC[47m"
+BG_BLACK="$_ESC[40m"
